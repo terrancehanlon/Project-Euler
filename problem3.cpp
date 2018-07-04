5c4183426764363979b88278013ebc39dfa494b5
Terrance <terrance.hanlon@ndsu.edu>
2018-07-03 19:45:13 -0500

    problem 3

--- problem3.cpp
+++ problem3.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<vector>
+#include<math.h>
+#include <stdio.h>
 
 using namespace std;
 
@@ -21,32 +23,53 @@ bool checkIfPrime(long n)
     }
     return true;
 }
-int primeCheck(long n)
+
+
+
+void primeFactors(long n)
 {
-    int maxPrime = 0;
-    for(int i = 2; i < n; i++)
+    //All prime factors have a 2, get the number of 2s by dividing by 2
+    while(n % 2 == 0)
     {
-        if(i % 2 == 0)
-        {
-            continue;
-        }
-       if(checkIfPrime(i))
-       {
-           if(i > maxPrime)
-           {
-               maxPrime = i;
-           }
-       }
+        //printf("%d", 2);
+        cout<<2<<" ";
+        //if can be divided 2, obv means it can be multiplied by 2
+        n = n/2;
     }
 
-    return maxPrime;
+    //By continually dividing by 2 we will end up with an odd number
+    //difference pf two prime numbers must be 2, therefore the only even number in prime factor is 2 so we can skip evens
+    for(int i = 3; i <= sqrt(n); i+=2)
+    {
+        //same logic as first while loop, but now looking for odd prime factors
+        while(n%i == 0)
+        {
+            //printf("%d", i);
+            cout<<i<<" ";
+            n = n/i;
+        }
+
+    }
+
+    //if for any reason now n < 2, it must be prime
+    if(n > 2)
+    {
+        //printf("%d", n);
+        cout<<n<<" ";
+    }
 }
 
 
 int main()
 {
-    int x = primeCheck(600851475143);
-    cout<<x;
+    //printPrimes(13195);
+    //bool x = checkIfPrime(10);
+    //cout<<"\n"<<!x;
+
+//largestPrime(10);
+    //cout<< x;
     
+    primeFactors(600851475143);
+    cout<<endl;
     return 0;
 }
